00e20a09023a24901e212df180bba396e27bdc4f
Turkey8008 <sluther@guilford.edu>
2022-08-30 11:59:24 -0400

    Save

---
+++ testscript.js
@@ -0,0 +1,33 @@
+// Describes what should be stored as myitem
+myitem = document.getElementById("noindent2");
+// Adds an event which occurs when myitem is clicked.
+myitem.addEventListener("click", onClick);
+// Uses the event as a catalyst for other changes
+function onClick() {  
+    // Changes the color of myitem to green when it is clicked
+    myitem.style.color = "green";
+    myitem.style.fontFamily = "Courier", "Times", "Times New Roman";  
+}
+myitem.addEventListener("mouseout", onMouseOut);
+
+function onMouseOut() { 
+    myitem.style.color = "blue";
+    myitem.style.fontFamily = ""
+}
+
+thebutton = document.getElementById("thebutton");  
+otheritem = document.getElementById("buttontest");
+
+thebutton.addEventListener("click", onButtonClick);
+
+function onButtonClick() {  
+    otheritem.style.color = "red";  
+}
+
+textentry = document.getElementById("myinput");  
+textentry.addEventListener("change", onChange);
+
+function onChange() {  
+    newtext = myinput.value;  
+    otheritem.innerHTML = newtext;  
+}
